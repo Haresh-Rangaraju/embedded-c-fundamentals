298410067db09eff9fdc0f79b869e7114265cb13
Haresh R <hareshrangaraju@gmail.com>
2026-06-22 20:03:21 +0530

    add 7_strings.c

---
+++ 8 Strings/7_strings.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    char name[]="ECU";
+    char cmd[]="START";
+    char msg[20]="ERROR";
+    printf("Name = %s\n", name);
+    printf("Command = %s\n", cmd);
+    printf("Message = %s\n", msg);
+
+    return 0;
+}
